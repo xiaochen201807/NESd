116545b534a3ada6ad4f6f98f1d5629c360b2e1c
xiaochen <xiaochen1649@gmail.com>
2026-06-25 20:59:53 +0800

    feat: force landscape orientation for entire NesdApp

--- packages/nesd/lib/ui/nesd_app.dart
+++ packages/nesd/lib/ui/nesd_app.dart
@@ -1,16 +1,35 @@
+import 'package:flutter/services.dart';
+import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:nesd/ui/router/router.dart';
 import 'package:nesd/ui/router/router_observer.dart';
 import 'package:nesd/ui/settings/settings.dart';
 import 'package:nesd/ui/theme/dark.dart';
 import 'package:nesd/ui/theme/light.dart';
 
-class NesdApp extends ConsumerWidget {
+class NesdApp extends HookConsumerWidget {
   const NesdApp({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    useEffect(() {
+      WidgetsBinding.instance.addPostFrameCallback((_) {
+        SystemChrome.setPreferredOrientations([
+          DeviceOrientation.landscapeLeft,
+          DeviceOrientation.landscapeRight,
+        ]);
+      });
+      return () {
+        WidgetsBinding.instance.addPostFrameCallback((_) {
+          SystemChrome.setPreferredOrientations([
+            DeviceOrientation.portraitUp,
+          ]);
+        });
+      };
+    }, const []);
+
     final observer = ref.watch(routerObserverProvider.notifier);
     final router = ref.watch(routerProvider);
     final themeMode = ref.watch(
