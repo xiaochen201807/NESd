3d7b8a643f3bc4e0728f868a1acc7ff45e20f5e8
xiaochen <xiaochen1649@gmail.com>
2026-06-25 20:56:13 +0800

    style: reduce touch controller opacity for floating overlay look

--- packages/nesd/lib/ui/emulator/input/touch/touch_controls.dart
+++ packages/nesd/lib/ui/emulator/input/touch/touch_controls.dart
@@ -7,9 +7,9 @@ import 'package:nesd/ui/emulator/input/touch/rectangle_button.dart';
 import 'package:nesd/ui/emulator/input/touch/touch_input_config.dart';
 import 'package:nesd/ui/settings/settings.dart';
 
-const touchInputColorActive = Color(0xeeaaaaaa);
-const touchInputColor = Color(0xbbaaaaaa);
-const touchInputColorSecondary = Color(0x66aaaaaa);
+const touchInputColorActive = Color(0xaa888888);
+const touchInputColor = Color(0x55aaaaaa);
+const touchInputColorSecondary = Color(0x33aaaaaa);
 
 class TouchControlsBuilder extends ConsumerWidget {
   const TouchControlsBuilder({this.edit = false, super.key});
